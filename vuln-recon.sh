c9865560e49e358e3909f902a1064bbc6845e916
head-pain <jeesepinkman1919@gmail.com>
2025-04-23 18:55:10 +0300

    vuln-recon.sh

--- vuln-recon.sh
+++ vuln-recon.sh
@@ -64,18 +64,88 @@ fi
 
 echo -e $lpurp"[+] The (httpx-toolkit) Done...!!\n"$white
 
-echo -e $blue"[*] We Use (waybackurls) For Find vulns...!!\n"$white
+echo -e $blue"[*] We Use (waybackurls, gau) For Find vulns...!!\n"$white
 
-echo -e $black"[*] If The Tool Done Close The Terminal...!!\n"$white
+clear
 
-konsole -e bash -c "sleep 1; xdotool type 'cat live-subdomain.txt | waybackurls | gf xss | kxss | tee xss.txt'; xdotool key Return; exec bash"
+echo -e $pruple"[*] Type This In Terminal >>> (cat live-subdomain.txt | waybackurls | tee urls-waybackurls.txt) \n"
 
-konsole -e bash -c "sleep 1; xdotool type 'cat live-subdomain.txt | waybackurls | gf lfi | tee lfi.txt'; xdotool key Return; exec bash"
+echo -e "[*] Type This In Terminal >>> (cat live-subdomain.txt | gau | tee urls-gau.txt) \n"$white
 
-konsole -e bash -c "sleep 1; xdotool type 'cat live-subdomain.txt | waybackurls | gf sqli | tee sqli.txt'; xdotool key Return; exec bash"
+read -p $red"Do You Understand..? (Y/n): " response
+$white
+if [[ "$response" == "y" || "$response" == "" || "$response" == "Y" ]]; then
+  count=1
 
-konsole -e bash -c "sleep 1; xdotool type 'cat live-subdomain.txt | waybackurls | gf upload-fields | tee upload-fields.txt'; xdotool key Return; exec bash"
+while [ $count -le 1 ]; do
 
-echo -e $pruple"[+] The (waybackurls) Done...!!\n"$white
+    konsole -e bash -c "sleep 1; xdotool type ''; xdotool key Return; exec bash" | konsole -e bash -c "sleep 1; xdotool type ''; xdotool key Return; exec bash"
 
-echo -e $green"[/] All Works Is Done...!!"$white
+    clear
+
+    echo -e $pruple"[*] Type This In Terminal >>> (cat live-subdomain.txt | waybackurls | tee urls-waybackurls.txt) \n"
+
+    echo -e "[*] Type This In Terminal >>> (cat live-subdomain.txt | gau | tee urls-gau.txt) \n"$white
+
+    sort -u urls-waybackurls.txt urls-gau.txt | tee url.txt
+
+    cat url.txt | grep "=" | tee params.txt
+
+    count=$((count + 1))
+done
+elif [[ "$response" == "n" ]]; then
+  exit
+else
+  echo "Invalid input. Please enter 'y' or 'n'."
+fi
+
+echo -e $lpurp"[+] The (waybackurls, gau) Done...!!\n"$white
+
+mkdir vulns
+
+echo -e $blue"[*] We Use (gf -list) For Find vulns...!!\n"$white
+
+clear
+
+echo -e $pruple"[*] Type This For $blue (XSS) $pruple In Terminal >>> (cat params.txt | gf xss | tee vulns/potential-xss.txt) \n $red IF This Done Type This \n $pruple (cat potential-xss.txt | xargs -n1 -P 20 kxss > vulns/reflected-xss.txt) \n"
+
+echo -e "[*] Type This $blue (LFI) $pruple In Terminal >>> (cat params.txt | gf lfi | tee vulns/lfi.txt) \n"$white
+
+echo -e "[*] Type This $blue (SQLI) $pruple In Terminal >>> (cat params.txt | gf sqli | tee vulns/sqli.txt) \n"$white
+
+echo -e "[*] Type This $blue (UPLOUD-FILE) $pruple In Terminal >>> (cat params.txt | gf upload-fields | tee vulns/upload-fields.txt) \n"$white
+
+echo -e "[*] Type This $blue (REDIRECT) $pruple In Terminal >>> (cat params.txt | grep '=http' | qsreplace 'https://evil.com' | httpx-toolkit -silent -fr -location | grep 'evil.com' | tee vulns/openredirect.txt) \n"$white
+
+read -p $red"Do You Understand..? (Y/n): " response
+$white
+if [[ "$response" == "y" || "$response" == "" || "$response" == "Y" ]]; then
+  count=1
+
+while [ $count -le 1 ]; do
+
+    konsole -e bash -c "sleep 1; xdotool type ''; xdotool key Return; exec bash" | konsole -e bash -c "sleep 1; xdotool type ''; xdotool key Return; exec bash" | konsole -e bash -c "sleep 1; xdotool type ''; xdotool key Return; exec bash" | konsole -e bash -c "sleep 1; xdotool type ''; xdotool key Return; exec bash" | konsole -e bash -c "sleep 1; xdotool type ''; xdotool key Return; exec bash" 
+
+    clear
+
+    echo $red"[-] I Know You Didn't UndersTanding...!!\n"$white
+
+    echo -e $pruple"[*] Type This For $blue (XSS) $pruple In Terminal >>> (cat params.txt | gf xss | tee vulns/potential-xss.txt) \n $red IF This Done Type This \n $pruple (cat potential-xss.txt | xargs -0 -P 20 kxss > vulns/reflected-xss.txt) \n"
+
+    echo -e "[*] Type This $blue (LFI) $pruple In Terminal >>> (cat params.txt | gf lfi | tee vulns/lfi.txt) \n"$white
+
+    echo -e "[*] Type This $blue (SQLI) $pruple In Terminal >>> (cat params.txt | gf sqli | tee vulns/sqli.txt) \n"$white
+
+    echo -e "[*] Type This $blue (UPLOUD-FILE) $pruple In Terminal >>> (cat params.txt | gf upload-fields | tee vulns/upload-fields.txt) \n"$white
+
+    echo -e "[*] Type This $blue (REDIRECT) $pruple In Terminal >>> (cat params.txt | grep '=http' | qsreplace 'https://evil.com' | httpx-toolkit -silent -fr -location | grep 'evil.com' | tee vulns/openredirect.txt) \n"$white
+
+    count=$((count + 1))
+done
+elif [[ "$response" == "n" ]]; then
+  exit
+else
+  echo "Invalid input. Please enter 'y' or 'n'."
+fi
+
+echo -e $green"[+] All Tasks Done..!!"$white
